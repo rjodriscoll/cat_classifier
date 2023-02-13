8c195c2bc0abea2490c760c48a41b5a711b0c024
RJODRISCOLL <ruairiodriscoll@hotmail.co.uk>
2023-02-13 23:03:28 +0000

    initial vgg cnn

---
+++ cclf/src/utils.py
@@ -0,0 +1,8 @@
+
+import os
+
+
+def print_dir(dir_path):
+  for dirpath, dirnames, filenames in os.walk(dir_path):
+    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+
