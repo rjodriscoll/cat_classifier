e61e2966fe5670a36ad0163d1b363d21de7e3e51
RJODRISCOLL <ruairiodriscoll@hotmail.co.uk>
2023-02-18 22:27:58 +0000

    cat clf running

--- cclf/src/utils.py
+++ cclf/src/utils.py
@@ -1,8 +1,30 @@
 
 import os
+import matplotlib.pyplot as plt
 
 
 def print_dir(dir_path):
   for dirpath, dirnames, filenames in os.walk(dir_path):
     print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
 
+def loss_curves(results):
+   
+    epochs = range(len(results["train_loss"]))
+
+    plt.figure(figsize=(15, 7))
+
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, results["train_loss"], label="train_loss")
+    plt.plot(epochs, results["test_loss"], label="test_loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, results["train_acc"], label="train_accuracy")
+    plt.plot(epochs,  results["test_acc"], label="test_accuracy")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
